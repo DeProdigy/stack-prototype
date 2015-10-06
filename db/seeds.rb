a3a2cfedb3ed89f45f400babbe157e971aec22f8
DeProdigy <alex.hint.ah@gmail.com>
2015-10-06 16:44:55 -0400

    Create seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+Post.destroy_all
+
+User.create( username: 'Jano', posts: [
+    Post.create( content: 'https://medium.com/@dhh/there-s-always-time-to-launch-your-dream-30f80826739d' ),
+    Post.create( content: 'https://www.youtube.com/watch?v=T24_uq0AY6o' ),
+    Post.create( content: 'https://soundcloud.com/just-blaze/just-blaze-x-dj-soul-casino' )
+  ]);
+
+User.create( username: 'Nacho', posts: [
+    Post.create( content: 'https://dribbble.com/shots/2278322-Adding-a-new-goal-animation' ),
+    Post.create( content: 'https://medium.com/@hintology/the-ten-commandments-of-tech-culture-a903f5a4da1a' )
+  ]);
+
+User.create( username: 'Blanco', posts: [
+    Post.create( content: 'https://twitter.com/2CentCap/status/649259335411961856' ),
+    Post.create( content: 'https://www.facebook.com/video.php?v=10153685866543896' ),
+    Post.create( content: 'http://theserhythmsthissoul.tumblr.com/' ),
+    Post.create( content: 'https://www.youtube.com/watch?v=stD9RycSXiU' ),
+    Post.create( content: 'http://mashable.com/2015/10/06/surface-book-vs-macbook-pro/' )
+  ]);
